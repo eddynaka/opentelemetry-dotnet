5b94e5e94fba57f6af866977e4895ee9a72869dd
Eddy Nakamura <ednakamu@microsoft.com>
2020-07-25 12:11:45 -0300

    updating tests

--- src/OpenTelemetry.Shims.OpenTracing/TracerShim.cs
+++ src/OpenTelemetry.Shims.OpenTracing/TracerShim.cs
@@ -115,8 +115,7 @@ namespace OpenTelemetry.Shims.OpenTracing
 
             if ((format == BuiltinFormats.TextMap || format == BuiltinFormats.HttpHeaders) && carrier is ITextMap textMapCarrier)
             {
-                // Remove comment after spanshim changes
-                // this.textFormat.Inject(shim.SpanContext, textMapCarrier, (instrumentation, key, value) => instrumentation.Set(key, value));
+                this.textFormat.Inject(shim.SpanContext, textMapCarrier, (instrumentation, key, value) => instrumentation.Set(key, value));
             }
         }
     }
